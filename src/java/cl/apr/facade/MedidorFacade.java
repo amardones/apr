83949d0f9150f96a7a1fce92a6b4adf892e43986
amardones <ariel.mardones@gmail.com>
2015-11-08 20:19:44 -0400

    test

--- src/java/cl/apr/facade/MedidorFacade.java
+++ src/java/cl/apr/facade/MedidorFacade.java
@@ -6,9 +6,11 @@
 package cl.apr.facade;
 
 import cl.apr.entity.Medidor;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -28,4 +30,19 @@ public class MedidorFacade extends AbstractFacade<Medidor> {
         super(Medidor.class);
     }
     
+    public List<Medidor> getMedidoresDisponibles() {
+       /*
+         javax.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+        cq.select(cq.from(Medidor.class));
+        return getEntityManager().createQuery(cq).getResultList();
+        */
+         //NOT IN (SELECT numero_medidor FROM cuenta 
+         //SELECT numero_medidor, descripcion FROM Medidor WHERE numero_medidor NOT IN (SELECT numero_medidor FROM cuenta )
+         //TypedQuery<Medidor> query = em.createQuery(" SELECT m FROM Medidor m WHERE m.numeroMedidor = :numeroMedidor" , Medidor.class);
+         Query query = em.createQuery(""
+                                        + "SELECT m FROM Cuenta c RIGHT JOIN c.numeroMedidor m WHERE c.idCuenta IS NULL ", Medidor.class);
+         //query.setParameter("numeroMedidor","00001");
+         return query.getResultList();
+       
+    }
 }
