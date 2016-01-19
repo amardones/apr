1056380dc287e6ffa6c337bd3e79c2338714b759
mmardones <matyas336699@gmail.com>
2016-01-19 00:14:27 -0300

    Funcionando

---
+++ src/java/cl/apr/beans/SubsidioReporte.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.apr.beans;
+
+import cl.apr.entity.Cuenta;
+import cl.apr.entity.Periodo;
+
+/**
+ *
+ * @author Matias
+ */
+public class SubsidioReporte {  
+
+
+    private Integer idPeriodo;
+    private Integer idcuenta;   
+    private String fechaCreacion;
+    private Integer descuento_periodo;
+    
+    public SubsidioReporte(){
+        
+    }
+
+    public Integer getIdPeriodo() {
+        return idPeriodo;
+    }
+
+    public void setIdPeriodo(Integer idPeriodo) {
+        this.idPeriodo = idPeriodo;
+    }
+
+    public Integer getIdcuenta() {
+        return idcuenta;
+    }
+
+    public void setIdcuenta(Integer idcuenta) {
+        this.idcuenta = idcuenta;
+    }
+
+  
+    
+
+   
+    
+
+   
+
+    public String getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(String fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Integer getDescuento_periodo() {
+        return descuento_periodo;
+    }
+
+    public void setDescuento_periodo(Integer descuento_periodo) {
+        this.descuento_periodo = descuento_periodo;
+    }
+    
+}
+
