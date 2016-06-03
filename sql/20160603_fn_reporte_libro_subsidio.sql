0871e79ac8476a9fdfe5d1ee4aa8308635ffcdaa
mmardones <matyas336699@gmail.com>
2016-06-03 00:30:25 -0400

    reporte subsidio se agrego nueva columna

--- sql/20160603_fn_reporte_libro_subsidio.sql
+++ sql/20160603_fn_reporte_libro_subsidio.sql
@@ -2,7 +2,9 @@
 
 -- DROP FUNCTION fn_reporte_libro_subsidio(date, date);
 
-CREATE OR REPLACE FUNCTION fn_reporte_libro_subsidio("fecha_inicio$" date, "fecha_fin$" date)
+CREATE OR REPLACE FUNCTION fn_reporte_libro_subsidio(
+    "fecha_inicio$" date,
+    "fecha_fin$" date)
   RETURNS refcursor AS
 $BODY$
 	DECLARE
@@ -10,16 +12,19 @@ $BODY$
 	BEGIN 
       
 	OPEN p_return FOR
-			SELECT 
+		SELECT 
 			DISTINCT 
 			a.id_cuenta AS CUENTA
 			,a.id_periodo AS PERIODO
-			,to_char(a.fecha_creacion, 'dd/MM/yyyy')
-			,descuento_periodo AS DESCUENTO
+			,to_char(a.fecha_creacion, 'dd/MM/yyyy') 
+			,d.descuento AS DESCUENTO
+			,(CASE WHEN  d.descripcion like '%SUBSIDIO%' THEN 'EXTERNO' ELSE 'INTERNO' END) AS tipo
+
+			 
 					FROM aviso_cobro a INNER JOIN detalle_aviso_cobro d ON a.id_periodo=d.id_periodo AND a.id_cuenta=d.id_cuenta 
-					AND d.descripcion like '%SUBSIDIO%'
-					WHERE descuento_periodo <> 0 AND
-						fecha_creacion::DATE >= fecha_inicio$ AND  
+					
+					WHERE d.descuento <> 0 
+					AND fecha_creacion::DATE >= fecha_inicio$ AND  
 						fecha_creacion::DATE <= fecha_fin$;
 			
 	RETURN p_return;
